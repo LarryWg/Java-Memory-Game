ba3e9c9d015d0e392d517d1dfc68f5e1d0f657d3
Larry Wang <lwang20@ocdsb.ca>
2023-04-24 19:21:54 -0400

    Created main menu page

--- src/main/MemoryTest.java
+++ src/main/MemoryTest.java
@@ -1,12 +1,8 @@
+package src.main;
+
 public class MemoryTest{
   
-  
-  
-  
-  
-  
-  
   public static void main(String[] args){
-    
-  
+    new Menu();
+  }
 }
---
+++ src/main/Menu.java
@@ -0,0 +1,47 @@
+package src.main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Menu extends JFrame {
+    private JButton playButton;
+
+    public Menu() {
+        super("Memory Test");
+        setSize(1280,720);
+        setLocationRelativeTo(null);
+        setBackground(new Color(0x007AFF));
+        setLayout(null);
+        setResizable(false);
+
+        JLabel titleLabel = new JLabel("Memory Test");
+        titleLabel.setFont(new Font("Helvetica Neue", Font.BOLD, 150));
+        titleLabel.setForeground(new Color(0xFFFFFF));
+        titleLabel.setBounds(180,100,1000,300);
+
+        playButton = new JButton("Play");
+        playButton.setFont(new Font("Helvetica Neue", Font.BOLD, 40));
+        playButton.setBackground(new Color(255,209,84));
+        playButton.setForeground(new Color(0x242424));
+        playButton.setFocusPainted(false);
+        playButton.setBounds(540,520,200,60);
+       
+        
+
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        buttonPanel.setOpaque(false);
+        buttonPanel.add(playButton);
+
+        JPanel menuPanel = new JPanel(new BorderLayout());
+        menuPanel.setLayout(null);
+        menuPanel.setOpaque(false);
+        menuPanel.add(titleLabel);
+        menuPanel.add(playButton);
+
+        setContentPane(menuPanel);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+
+    }
+}
